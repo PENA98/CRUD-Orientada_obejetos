1a007b44e9a49d4e88354832c7343a554c23e827
PENA98 <maynorx5000@gmail.com>
2018-10-26 15:13:14 -0600

    1

---
+++ Departamentos/Departamentos/Acciones_Departamentos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// namespaces necesarios
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Departamentos
+{
+    class Acciones_Departamentos
+    {
+        protected SqlConnection conn = new SqlConnection(@"server = (local)\sqlexpress;
+            integrated security = true; database = AdventureWorks2014");
+        protected string sql;
+
+        public Acciones_Departamentos() { }
+
+
+    }
+}
